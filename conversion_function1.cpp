6b0fdf7409fef2af691a3ed692b8bf85b8942857
YukiAsano <yuuki.1224.softtennis@gmail.com>
2014-11-03 22:43:07 +0900

    update conversion_function1.cpp

--- conversion_function1.cpp
+++ conversion_function1.cpp
@@ -2,14 +2,17 @@
 using namespace std;
 
 class coord {
+
     int x, y;
     public:
     coord(int i, int j) { x = i; y = j; }
     // 変換関数の定義
     operator int() { return x*y; }
+
 };
 
 int main(int argc, const char * argv[]) {
+
     coord o1(2, 3), o2(4, 3);
     int i;
 
